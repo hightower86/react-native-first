b7a71e46050701724ab2695d107b782a319bd946
arezvanov@arezvanov <htwr86@gmail.com>
2024-05-06 16:29:38 +0400

    01.2 tabs layout

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -1,11 +1,114 @@
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import React from 'react';
+import { Tabs, Redirect } from 'expo-router';
+// import { Home, Create, Bookmark, Profile } from '../(tabs)';
+
+import { icons } from '../../constants';
+
+const TabIcon = ({ icon, color, name, focused }) => {
+    return (
+        <View className="items-center justify-center gap-2">
+            <Image
+                source={icon}
+                resizeMode="contain"
+                tintColor={color}
+                className="w-6 h-6"
+            />
+            <Text
+                className={` ${
+                    focused ? 'font-psemibold' : 'font-pregular'
+                } text-xs`}
+                style={{ color: color }}
+            >
+                {name}
+            </Text>
+        </View>
+    );
+};
 
 const TabsLayout = () => {
     return (
-        <View>
-            <Text>TabsLayout</Text>
-        </View>
+        <>
+            <Tabs
+                screenOptions={{
+                    tabBarShowLabel: false,
+                    tabBarActiveTintColor: '#FFA001',
+                    tabBarInactiveTintColor: '#CDCDE0',
+                    tabBarStyle: {
+                        backgroundColor: '#161622',
+                        borderTopWidth: 1,
+                        borderTopColor: '#232533',
+                        height: 84,
+                    },
+                }}
+            >
+                <Tabs.Screen
+                    name="home"
+                    options={{
+                        title: 'Home',
+                        headerShown: false,
+                        tabBarIcon: ({ focused, color }) => (
+                            <TabIcon
+                                icon={icons.home}
+                                color={color}
+                                name="Home"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                    // component={Home}
+                />
+
+                <Tabs.Screen
+                    name="bookmark"
+                    options={{
+                        title: 'Bookmark',
+                        headerShown: false,
+                        tabBarIcon: ({ focused, color }) => (
+                            <TabIcon
+                                icon={icons.bookmark}
+                                color={color}
+                                name="Bookmark"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                    // component={Bookmark}
+                />
+                <Tabs.Screen
+                    name="create"
+                    options={{
+                        title: 'Create',
+                        headerShown: false,
+                        tabBarIcon: ({ focused, color }) => (
+                            <TabIcon
+                                icon={icons.plus}
+                                color={color}
+                                name="Create"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                    // component={Create}
+                />
+                <Tabs.Screen
+                    name="profile"
+                    options={{
+                        title: 'Profile',
+                        headerShown: false,
+                        tabBarIcon: ({ focused, color }) => (
+                            <TabIcon
+                                icon={icons.profile}
+                                color={color}
+                                name="Profile"
+                                focused={focused}
+                            />
+                        ),
+                    }}
+                    // component={Profile}
+                />
+            </Tabs>
+        </>
     );
 };
 
