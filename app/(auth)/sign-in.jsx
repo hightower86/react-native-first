71d77511b335503130f0c259cdf02356204588bc
arezvanov@arezvanov <htwr86@gmail.com>
2024-05-07 07:31:46 +0400

    01.4 sign in screen view

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,11 +1,25 @@
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView, Image } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from '../../constants';
 
 const SignIn = () => {
     return (
-        <View>
-            <Text>SignIn</Text>
-        </View>
+        <SafeAreaView className="bg-primary h-full">
+            <ScrollView>
+                <View className="min-h-[85vh] w-full px-4 items-center justify-center">
+                    <Image
+                        source={images.logo}
+                        resizeMode="contain"
+                        className="w-[115px] h-[35px]"
+                    />
+                    <Text className="text-white text-2xl text-semibold mt-10 font-psemibold text-center">
+                        Log in to Aora
+                    </Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     );
 };
 
